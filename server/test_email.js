c5ce9902e6a15f6d56373434c3d23b38aa67e87f
AbhishekBogahawaththa <abihisheakbogahawaththa@gmail.com>
2026-05-04 10:28:54 +0530

    fix corrupted index.js and registerRoutes.js

---
+++ server/test_email.js
@@ -0,0 +1,20 @@
+﻿const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'abihisheakbogahawaththa@gmail.com',
+    pass: 'xvhpehwdkiqzsasp',
+  },
+});
+
+transporter.sendMail({
+  from: 'abihisheakbogahawaththa@gmail.com',
+  to: 'abihisheakbogahawaththa@gmail.com',
+  subject: 'XenEdu Email Test',
+  text: 'Email is working!',
+}).then(() => {
+  console.log('Email sent successfully!');
+}).catch(err => {
+  console.log('Error:', err.message);
+});
